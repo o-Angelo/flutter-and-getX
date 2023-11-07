ca58688c5330ba70e886184eef10551cb7e04749
o-Anjelo <angeloban92@gmail.com>
2023-11-07 18:19:23 -0300

    SignUp View | Created

--- lib/src/auth/sign_in_screen.dart
+++ lib/src/auth/sign_in_screen.dart
@@ -2,6 +2,7 @@ import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 
 import 'package:quitanda/src/auth/components/custom_text_field.dart';
+import 'package:quitanda/src/auth/sign_up_screen.dart';
 import 'package:quitanda/src/config/custom_colors.dart';
 
 class SignInScreen extends StatelessWidget {
@@ -166,7 +167,15 @@ class SignInScreen extends StatelessWidget {
                               width: 2,
                               color: Colors.green,
                             )),
-                        onPressed: () {},
+                        onPressed: () {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (c) {
+                                return SignUpScreen();
+                              },
+                            ),
+                          );
+                        },
                         child: const Text(
                           'Criar conta',
                           style: TextStyle(
