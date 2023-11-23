24fb506796e1ba97312b518d0eaaedaa148ea172
o-Anjelo <angeloban92@gmail.com>
2023-11-23 14:07:38 -0300

    Profile UI | Change Password Dialogi

--- lib/src/pages/profile/profile_tab.dart
+++ lib/src/pages/profile/profile_tab.dart
@@ -2,9 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:quitanda/src/pages/common_widgets/custom_text_field.dart';
 import 'package:quitanda/src/config/app_data.dart' as appData;
 
-class ProfileTab extends StatelessWidget {
+class ProfileTab extends StatefulWidget {
   const ProfileTab({super.key});
 
+  @override
+  State<ProfileTab> createState() => _ProfileTabState();
+}
+
+class _ProfileTabState extends State<ProfileTab> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -60,7 +65,9 @@ class ProfileTab extends StatelessWidget {
           SizedBox(
             height: 50,
             child: OutlinedButton(
-              onPressed: () {},
+              onPressed: () {
+                updatePassword();
+              },
               style: OutlinedButton.styleFrom(
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(20),
@@ -75,5 +82,86 @@ class ProfileTab extends StatelessWidget {
         ],
       ),
     );
+  }
+
+  Future<bool?> updatePassword() {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return Dialog(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Stack(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      const Padding(
+                        padding: EdgeInsets.symmetric(vertical: 12),
+                        child: Text(
+                          'Atualização de Senha',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+
+                      // senha atual
+                      const CustomTextField(
+                        icon: Icons.lock,
+                        label: 'Senha Atual',
+                        isSecret: true,
+                      ),
+
+                      // nova senha
+                      const CustomTextField(
+                        icon: Icons.lock_outline,
+                        label: 'Nova Senha',
+                        isSecret: true,
+                      ),
+
+                      // confirmar nova senha
+                      const CustomTextField(
+                        icon: Icons.lock_outline,
+                        label: 'Confirmar Nova Senha',
+                        isSecret: true,
+                      ),
+
+                      // botao atualizar
+                      SizedBox(
+                        height: 45,
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                          ),
+                          child: const Text('Atualizar'),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Positioned(
+                  top: 5,
+                  right: 5,
+                  child: IconButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    icon: const Icon(Icons.close),
+                  ),
+                ),
+              ],
+            ),
+          );
+        });
   }
 }
